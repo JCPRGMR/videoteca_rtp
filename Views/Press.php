bee2ed0062347818b0c90966e956ec5cde2efaf4
JCPRGMR <juancardedon@gmail.com>
2024-07-02 12:54:18 -0400

    [02/07/2024 12:53] Se arreglo el bug de mostrar videos irreconocibles

--- Views/Press.php
+++ Views/Press.php
@@ -12,30 +12,32 @@
         </thead>
         <tbody id="tbody">
             <?php foreach(Videos::MostrarPrensa() as $item):?>
-                <tr class="odd8">
-                    <td class="center p10"><?= $item->des_area ?></td>
-                    <td class="center p10"><?= $item->des_kind ?></td>
-                    <td class="center p10"><?= $item->title ?></td>
-                    <td class="center p10"><?= $item->details ?></td>
-                    <td class="center p10"><?= date_format(new DateTime($item->date_user), "Y-m-d") ?></td>
-                    <td class="center p10 f-row">
-                        <?php if($item->path_play != null):?>
-                            <form action="Player.php" method="post">
-                                <button type="submit" name="ver_video" class="color2 negrita mayus" value="<?= $item->id_video ?>">Ver</button>
-                            </form>
-                            <?php if($_SESSION['usuario']["user_permission"] == "Administrador"):?>
+                <?php if($item->path_play != null): ?>
+                    <tr class="odd8">
+                        <td class="center p10"><?= $item->des_area ?></td>
+                        <td class="center p10"><?= $item->des_kind ?></td>
+                        <td class="center p10"><?= $item->title ?></td>
+                        <td class="center p10"><?= $item->details ?></td>
+                        <td class="center p10"><?= date_format(new DateTime($item->date_user), "Y-m-d") ?></td>
+                        <td class="center p10 f-row">
+                            <?php if($item->path_play != null):?>
+                                <form action="Player.php" method="post">
+                                    <button type="submit" name="ver_video" class="color2 negrita mayus" value="<?= $item->id_video ?>">Ver</button>
+                                </form>
+                                <?php if($_SESSION['usuario']["user_permission"] == "Administrador"):?>
+                                    <form action="../Php/VideoDelete.php" method="post" id="deleteForm">
+                                        <button type="submit" name="video" id="EliminarVideo" class="color4 negrita mayus txtwhite" value="<?= $item->id_video ?>">Eliminar</button>
+                                    </form>
+                                <?php endif;?>
+                            <?php else:?>
+                                Irreconocible...
                                 <form action="../Php/VideoDelete.php" method="post" id="deleteForm">
                                     <button type="submit" name="video" id="EliminarVideo" class="color4 negrita mayus txtwhite" value="<?= $item->id_video ?>">Eliminar</button>
                                 </form>
                             <?php endif;?>
-                        <?php else:?>
-                            Irreconocible...
-                            <form action="../Php/VideoDelete.php" method="post" id="deleteForm">
-                                <button type="submit" name="video" id="EliminarVideo" class="color4 negrita mayus txtwhite" value="<?= $item->id_video ?>">Eliminar</button>
-                            </form>
-                        <?php endif;?>
-                    </td>
-                </tr>
+                        </td>
+                    </tr>
+                <?php endif; ?>
             <?php endforeach;?>
         </tbody>
     </table>
